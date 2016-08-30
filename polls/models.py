652ee1473739c8a7976eb4aa252d2bc2db80c801
lmyfzx <lmyfzx@qq.com>
2016-08-31 00:31:53 +0800

    DJango教程 第2部分完

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(verbose_name='问题描述', max_length=200)
+    pub_date = models.DateTimeField(verbose_name='提交时间')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.short_description = '最新发布？'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(verbose_name='选项', max_length=200)
+    votes = models.IntegerField(verbose_name='票数', default=0)
+
+    def __str__(self):
+        return self.choice_text
